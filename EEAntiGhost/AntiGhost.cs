a12b7b4531830f50b6c34895b2f49f7e3bcddfdd
Yonom <sepehr1376@gmail.com>
2014-07-24 18:28:24 +0200

    Initial Commit

---
+++ EEAntiGhost/AntiGhost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using CupCake;
+using CupCake.Messages.Receive;
+using CupCake.Players;
+using PlayerIOClient;
+
+namespace EEAntiGhost
+{
+    public class AntiGhost : CupCakeMuffin
+    {
+        private readonly List<int> _unknownLeftIds = new List<int>();
+ 
+        protected override void Enable()
+        {
+            this.Events.Bind<LeftReceiveEvent>(this.OnLeft);
+            this.Events.Bind<AddPlayerEvent>(this.OnAdd);
+        }
+
+        private void OnLeft(object sender, LeftReceiveEvent e)
+        {
+            Player player;
+            this.PlayerService.TryGetPlayer(e.UserId, out player);
+            if (player == null)
+            {
+                this._unknownLeftIds.Add(e.UserId);
+            }
+        }
+
+        private void OnAdd(object sender, AddPlayerEvent e)
+        {
+            if (this._unknownLeftIds.Remove(e.Player.UserId))
+            {
+                var message = Message.Create(String.Empty, e.Player.UserId);
+                var ev = new LeftReceiveEvent(message);
+                this.Events.Raise(ev);
+            }
+        }
+    }
+}
